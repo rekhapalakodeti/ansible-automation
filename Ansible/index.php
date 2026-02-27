40f93abe4784c7cb5e29b1c06c0ce57ac37529ed
rekhapalakodeti <rekhapalakodeti114@gmail.com>
2026-02-27 09:21:17 +0530

    updated index file

--- Ansible/index.php
+++ Ansible/index.php
@@ -1,3 +1,10 @@
-<?php
-echo "<h1>Welcome to Rekha DevOps Server 🚀</h1>";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Website</title>
+</head>
+<body>
+  <h1>Hello from Rekha 🚀</h1>
+  <p>This server is configured using Ansible.</p>
+</body>
+</html>
